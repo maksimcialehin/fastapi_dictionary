b7235c245e0584394e5ded3854030d412433d9bd
maksimcialehin <madqfrog1863@gmail.com>
2022-12-23 21:40:28 +0300

    API query from dictionary

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Query
 from typing import List, Optional
 from fastapi.encoders import jsonable_encoder
 from model.db_handler import match_exact, match_like
+import uvicorn
 
 
 app = FastAPI()
@@ -17,21 +18,29 @@ def index():
 
 
 @app.get('/dict')
-def dictionary(word: str):
+def dictionary(words: List[str] = Query(None)):
+    print(words)
     
-    if not word:
-        response = {'status': 'error', 'word': word, 'data': 'word not found'}
+    if not words:
+        response = {'status': 'error', 'word': words, 'data': 'word not found'}
         return jsonable_encoder(response)
 
-    definitions = match_exact(word)
-    if definitions:
-        response = {'status': 'success', 'word': word, 'data': definitions}
-        return jsonable_encoder(definitions)
+    response = {'words': []}
 
-    definitions = match_like(word)
-    if definitions:
-        response = {'status': 'partial', 'word': word, 'data': definitions}
-        return jsonable_encoder(definitions)
+    for word in words:
+        print(word)
+        definitions = match_exact(word)
+        if definitions:
+            response['words'].append({'status': 'success', 'word': word, 'data': definitions})
+        else:
+            definitions = match_like(word)
+            if definitions:
+                response['words'].append({'status': 'partial', 'word': word, 'data': definitions})
+            else:
+                response['words'].append({'status': 'error', 'word': word, 'data': 'word not found'})
     
-    response = {'status': 'error', 'word': word, 'data': 'word not found'}
     return jsonable_encoder(response)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
