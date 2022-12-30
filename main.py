cd45ea76d299c0f574681a18c1bdb4f21212adaa
maksimcialehin <madqfrog1863@gmail.com>
2022-12-30 20:03:33 +0300

    adding CRUD

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import uvicorn
 from fastapi import FastAPI
-from datetime import datetime
-from typing import Optional
 from fastapi.encoders import jsonable_encoder
+
+from model.model import Task
+import model.task_handler as task_handler
 
 
 app = FastAPI()
@@ -9,24 +11,32 @@ app = FastAPI()
 
 @app.get('/api/tasks')
 async def get_tasks():
-    pass
+    return await task_handler.get_tasks()
 
 
 @app.get('/api/tasks/{id}')
 async def get_task(id: int):
-    pass
+    return await task_handler.get_tasks(id)
 
 
 @app.post('/api/task/create')
-async def create_task(task: Task):
-    pass
+async def create_task(new_task: Task):
+    id = await task_handler.create_task(new_task)
+    return await task_handler.get_tasks(id)
 
 
 @app.put('/api/tasks/{id}/update')
 async def update_task(id: int, task: Task):
-    pass
+    await task_handler.update_task(id, task)
+    return await task_handler.get_tasks(id)
 
 
 @app.delete('/api/tasks/{id}/delete')
 async def dalete_task(id: int, task: Task):
-    pass
+    deleted_id = await task_handler.delete_task(id)
+    response = {deleted_id: 'Task successfully deleted'}
+    return jsonable_encoder(response)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
--- model/task_handler.py
+++ model/task_handler.py
@@ -10,32 +10,27 @@ filepath = 'data/tasks.json'
 
 async def data_to_json(data: List):
     data = json.dumps(data)
+    print('JSON DATA', data)
     with open(filepath, 'w') as file:
         file.write(data)
 
 
-async def get_tasks(id: Optional[int]):
+async def get_tasks(id: Optional[int] = 0):
     tasks = parse_file_as(List[TaskList], filepath)
-    data = {task.id: task.sict() for task in tasks}
+    data = {task.id: task.dict() for task in tasks}
     response = data if id == 0 else data[id]
     return response
 
 
 async def create_task(new_task: Task):
+    print(f'NEW TASK: {new_task}')
     tasks = parse_file_as(List[TaskList], filepath)
     new_id = max([task.id for task in tasks]) + 1
     tasks.append(TaskList(id=new_id, task=new_task))
     data = [task.dict() for task in tasks]
+    print('DATA', data)
     await data_to_json(data)
     return new_id
-
-
-async def delete_task(id: int):
-    tasks = parse_file_as(List[TaskList], filepath)
-    tasks = [task for task in tasks if task.id != id]
-    data = [task.dict() for task in tasks]
-    await data_to_json(data)
-    return id
 
 
 async def update_task(id: int, new_task: Task):
@@ -44,5 +39,13 @@ async def update_task(id: int, new_task: Task):
     for task in data:
         if task.id == id:
             task.task = new_task.dict()
+    await data_to_json(data)
+    return id
+
+
+async def delete_task(id: int):
+    tasks = parse_file_as(List[TaskList], filepath)
+    tasks = [task for task in tasks if task.id != id]
+    data = [task.dict() for task in tasks]
     await data_to_json(data)
     return id
